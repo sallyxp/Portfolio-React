01f3ee5574e78f76445b6ea2e9ce3dea9b4c42f0
Sally Rodgers <sallyxp@yahoo.co.uk>
2021-06-15 21:35:11 +0100

    connections to different pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-//import { HashRouter as Router, Route } from "react-router-dom";
+import { HashRouter as Router, Route } from "react-router-dom";
 
 
 import logo from './logo.svg';
@@ -8,11 +8,29 @@ import Navbar from "./components/Navbar"
 import Wrapper from "./components/Wrapper";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import home from "./pages/home";
+import about from "./pages/about";
+import contact from "./pages/contact";
+import resume from "./pages/resume";
+import portfolio from "./pages/portfolio";
+
 function App() {
   return (
     <div className="App">
       <Wrapper>
+      <Router basename="Portfolio-React">
         <Navbar />
+    
+                  <Switch location={location}> 
+                 
+                    <Route exact path="/" component={home} />
+                    <Route exact path="/about" component={about} />
+                    <Route exact path="/contact" component={contact} />
+                    <Route exact path="/resume" component={resume} />
+                    <Route exact path="/portfolio" component={portfolio} />
+            
+                 </Switch>
+        </Router>
       </Wrapper>
     </div>
   );
