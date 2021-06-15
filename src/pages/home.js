920e074e0a0e2315b7b1337c67681467e4adbf6e
Sally Rodgers <sallyxp@yahoo.co.uk>
2021-06-15 12:39:19 +0100

    started pages - started home page

---
+++ src/pages/home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../styles/Home.css';
+import { NavLink } from 'react-router-dom';
+
+function Home() {
+    return (
+    <div className="jumbotron fade-in">
+    <h1 className="display-4"> Hello, I'm Sally Rodgers, </h1>  
+    <h2 className="blurb">Full-Stack Web Developer student.</h2>
+
+    <NavLink className="btn btn-outline-secondary btn-lg text-center" to="/about">Learn more about me</NavLink>
+    </div>
+    )
+};
+
+export default Home;
