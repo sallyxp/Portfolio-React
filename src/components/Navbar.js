4c71d2b90d82aa75618f9122cdff131aeb8000c1
Sally Rodgers <sallyxp@yahoo.co.uk>
2021-06-15 12:54:19 +0100

    added other links for pages

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 
 import React from 'react';
-import { navLink}  from "react-router-dom";
+import { navlink } from "react-router-dom";
 // import '../styles/Navbar.css';
 
 
@@ -14,18 +14,65 @@ function Navbar() {
             <div className="collapse navbar-collapse float-right" id="navbarNav">
                 <ul className="navbar-nav">
                     <li className="nav-item">
-                        <navLink
+                        <navlink
                             to="/"
                             className={
                                 window.location.pathname === "/" || window.location.pathname === "/"
                                     ? "nav-link active"
                                     : "nav-link"
-                            }>Home</navLink>
+                            }>Home</navlink>
                     </li>
+                        <li className="nav-item">
+                            <navlink
+                                to="/portfolio"
+                                className={
+                                    window.location.pathname === "/portfolio" || window.location.pathname === "/portfolio"
+                                        ? "nav-link active"
+                                        : "nav-link"
+                                }
+                            >Portfolio</navlink>
+                        </li>
+                        <li className="nav-item">
+                            <navlink
+                                to="/about"
+                                className={
+                                    window.location.pathname === "/about" || window.location.pathname === "/about"
+                                        ? "nav-link active"
+                                        : "nav-link"
+                                }
+                            >
+                                About</navlink>
+                        </li>
+
+                        <li className="nav-item">
+                            <navlink
+                                to="/contact"
+                                className={
+                                    window.location.pathname === "/contact" || window.location.pathname === "/contact"
+                                        ? "nav-link active"
+                                        : "nav-link"
+                                }
+                            >
+                                Contact</navlink>
+                        </li>
+
+                        <li className="nav-item">
+                            <navlink
+                                to="/resume"
+                                className={
+                                    window.location.pathname === "/resume" || window.location.pathname === "/resume"
+                                        ? "nav-link active"
+                                        : "nav-link"
+                                }
+                            >
+                                my CV
+                            </navlink>
+
+                        </li>
                 </ul>    
             </div>           
         </nav>
-            );
+    );
 }
 
             export default Navbar;
