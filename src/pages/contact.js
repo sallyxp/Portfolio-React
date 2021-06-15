562c507f0968c4d0f610a06eb26e80cb26d4f369
Sally Rodgers <sallyxp@yahoo.co.uk>
2021-06-15 21:35:49 +0100

    adding pages and content

---
+++ src/pages/contact.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function about() {
+    return (
+        <div className="bg-dark text-light page"id="page-content">
+            <Container classname="bio">
+            
+            </Container>
+        </div>
+
+        )
+};
+
+export default about;
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import '../styles/Home.css';
-import { NavLink } from 'react-router-dom';
+import '../styles/home.css';
+import { navLink } from 'react-router-dom';
 
-function Home() {
+function home() {
     return (
     <div className="jumbotron fade-in">
     <h1 className="display-4"> Hello, I'm Sally Rodgers, </h1>  
     <h2 className="blurb">Full-Stack Web Developer student.</h2>
 
-    <NavLink className="btn btn-outline-secondary btn-lg text-center" to="/about">Learn more about me</NavLink>
+    <navLink className="btn btn-outline-secondary btn-lg text-center" to="/about">Learn more about me</navLink>
     </div>
     )
 };
 
-export default Home;
+export default home;
