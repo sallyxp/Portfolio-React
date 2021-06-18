c5110c3cadc28d6285d437e1683649deaca20b3f
Sally Rodgers <sallyxp@yahoo.co.uk>
2021-06-18 23:07:00 +0100

    updated home page and navbar

--- src/pages/home.js
+++ src/pages/home.js
@@ -7,18 +7,20 @@ import portfolioImg from "../images/istockphoto-royalty-free535475263-612x612.jp
 
 function Home() {
     return (
-        
+
         <div className="jumbotron fade-in">
-           <Row>
-          <img src={portfolioImg} alt="Birmimgham" className="img-thumbnail" /> 
-             <h1 className=" flow-text display-4"> Hello, I'm Sally Rodgers, </h1>
-            <h2 className="blurb">Full-Stack Web Developer student.</h2>
-            </Row>
-          
-            <NavLink className="btn btn-outline-secondary btn-lg text-center" to="/about">Learn more about me</NavLink>
-        
+            <div className="container">
+                <Row>
+                    <img src={portfolioImg} alt="Birmimgham" className="img-thumbnail" />
+                    <h1 className=" flow-text display-4"> Hello, I'm Sally Rodgers, </h1>
+                    <h2 className="flow-text display-5">Full-Stack Web Developer student.</h2>
+                </Row>
+
+                <NavLink className="btn btn-outline-secondary btn-lg text-center" to="/about">Learn more about me</NavLink>
+            </div>
+
         </div>
-    )
+                )
 };
 
-export default Home;
+                export default Home;
